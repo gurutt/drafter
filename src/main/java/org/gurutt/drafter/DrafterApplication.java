4420a1b53f960dd6327222daba219b0b824f4377
Roman Tsymbalenko <rtsymbalenko@connecture.com>
2019-02-02 13:19:21 +0200

    Initial

---
+++ src/main/java/org/gurutt/drafter/DrafterApplication.java
@@ -0,0 +1,14 @@
+package org.gurutt.drafter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrafterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrafterApplication.class, args);
+	}
+
+}
+
